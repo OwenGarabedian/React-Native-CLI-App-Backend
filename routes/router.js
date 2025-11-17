b346f64ac0332ec5920cdb3dd56af67aeb200274
OwenGarabedian <owengarabedian@gmail.com>
2025-11-17 10:38:01 -0500

    Changed route name for more data routes in the future.

--- routes/router.js
+++ routes/router.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 // Corrected typo (router.get instead of roughter.get) and added a response
-router.get('/data', (req, res) => {
+router.get('/homescreen-data', (req, res) => {
     res.status(200).json({
         data: [
     'https://cdn.pixabay.com/photo/2021/01/06/07/32/leaf-5893399_1280.jpg',
@@ -14,7 +14,7 @@ router.get('/data', (req, res) => {
     'https://cdn.pixabay.com/photo/2021/01/06/07/32/leaf-5893399_1280.jpg',
     'https://cdn.pixabay.com/photo/2021/06/02/09/27/sunset-6303992_1280.jpg',
     'https://cdn.pixabay.com/photo/2023/03/17/20/59/vines-7859445_1280.jpg',
-] // Example data
+]
     });
 });
 
