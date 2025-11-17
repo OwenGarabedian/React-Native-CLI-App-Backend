38ec7a988cb74b202590e9c831f0979f43a3d1b0
OwenGarabedian <owengarabedian@gmail.com>
2025-11-17 10:41:02 -0500

    Added login route with example login codes.

--- routes/router.js
+++ routes/router.js
@@ -5,17 +5,26 @@ const router = express.Router();
 router.get('/homescreen-data', (req, res) => {
     res.status(200).json({
         data: [
-    'https://cdn.pixabay.com/photo/2021/01/06/07/32/leaf-5893399_1280.jpg',
-    'https://cdn.pixabay.com/photo/2021/06/02/09/27/sunset-6303992_1280.jpg',
-    'https://cdn.pixabay.com/photo/2023/03/17/20/59/vines-7859445_1280.jpg',
-    'https://cdn.pixabay.com/photo/2021/01/06/07/32/leaf-5893399_1280.jpg',
-    'https://cdn.pixabay.com/photo/2021/06/02/09/27/sunset-6303992_1280.jpg',
-    'https://cdn.pixabay.com/photo/2023/03/17/20/59/vines-7859445_1280.jpg',
-    'https://cdn.pixabay.com/photo/2021/01/06/07/32/leaf-5893399_1280.jpg',
-    'https://cdn.pixabay.com/photo/2021/06/02/09/27/sunset-6303992_1280.jpg',
-    'https://cdn.pixabay.com/photo/2023/03/17/20/59/vines-7859445_1280.jpg',
-]
+            'https://cdn.pixabay.com/photo/2021/01/06/07/32/leaf-5893399_1280.jpg',
+            'https://cdn.pixabay.com/photo/2021/06/02/09/27/sunset-6303992_1280.jpg',
+            'https://cdn.pixabay.com/photo/2023/03/17/20/59/vines-7859445_1280.jpg',
+            'https://cdn.pixabay.com/photo/2021/01/06/07/32/leaf-5893399_1280.jpg',
+            'https://cdn.pixabay.com/photo/2021/06/02/09/27/sunset-6303992_1280.jpg',
+            'https://cdn.pixabay.com/photo/2023/03/17/20/59/vines-7859445_1280.jpg',
+            'https://cdn.pixabay.com/photo/2021/01/06/07/32/leaf-5893399_1280.jpg',
+            'https://cdn.pixabay.com/photo/2021/06/02/09/27/sunset-6303992_1280.jpg',
+            'https://cdn.pixabay.com/photo/2023/03/17/20/59/vines-7859445_1280.jpg',
+        ]
     });
 });
 
+router.get('/login-data', (req, res) => {
+    res.status(200).json({
+        data: [
+            '123',
+            '789',
+            '000',
+        ]
+    });
+});
 module.exports = router;
